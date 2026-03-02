3c6dc6644ab8637439d6141a6066a2aacc9c52f2
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-02 15:05:10 -0500

    cmd params

--- W26Week8ConnectedModel/MainWindow.xaml.cs
+++ W26Week8ConnectedModel/MainWindow.xaml.cs
@@ -30,25 +30,50 @@ namespace W26Week8ConnectedModel
 
         private void LoadData()
         {
-            SqlConnection conn = new SqlConnection(connStr);
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "select EmployeeID, FirstName, LastName, City, Country from Employees";
+                SqlCommand cmd = new SqlCommand(query, conn);
 
-            string query = "select EmployeeID, FirstName, LastName, City, Country from Employees";
-            SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
 
-            conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
 
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            DataTable tbl = new DataTable();
-            tbl.Load(reader);
-            grdEmployees.ItemsSource = tbl.DefaultView;
-
-            conn.Close();
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
+            //conn.Close();
         }
 
         private void btnLoad_Click(object sender, RoutedEventArgs e)
         {
             LoadData();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            // string concatenation - AVOID
+            // allows sql injection
+            //string query = "select EmployeeID, FirstName, LastName, City, Country from Employees where FirstName='" + txtFirstname.Text + "'";
+
+            // command parameters - RECOMMENDED
+            // parameterized query
+            string query = "select EmployeeID, FirstName, LastName, City, Country from Employees where FirstName=@fn";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("fn", txtFirstname.Text);
+
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
         }
     }
 }
